225fd5ea0199e67996c98db3c143305ca7b33621
Gvido Bērziņš <gvido.berzins@testdevlab.com>
2022-05-17 17:55:31 +0300

    feat: Update exception test with catching generic Exception's specific message

--- how2test/exceptions_test.py
+++ how2test/exceptions_test.py
@@ -1,6 +1,14 @@
 import pytest
 
 
+def validate_string(value: str) -> bool:
+    """Validation function with the generic Exception"""
+    if isinstance(value, str) and len(value) > 0:
+        return True
+    else:
+        raise Exception(f"Invalid value given '{value}', expected a non-empty string")
+
+
 def parse_line(line: str, sep: str = " ") -> tuple[int]:
     """Parse given line into a tuple of integers"""
     if not line:
@@ -38,3 +46,9 @@ def test_parse_line_invalid_case_exception(line):
 def test_parse_line_invalid_case_exception_second_style(line):
     with pytest.raises(ValueError):
         parse_line(line)
+
+
+@pytest.mark.parametrize("value", (int, 1, None, (), dict(), "", [1, 2]))
+def test_validate_string(value):
+    with pytest.raises(Exception, match=r"Invalid.*non-empty string"):
+        validate_string(value)
